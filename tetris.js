f95bd31a4ee65843062a03dc1ead0b1064817afb
oz-hernandez <ohernandez@alumni.berklee.edu>
2017-02-25 13:45:28 -0500

    piece animation

--- tetris.js
+++ tetris.js
@@ -40,7 +40,8 @@ var I_SHAPE =		[
 							[0,0,1,0,0],
 							[0,0,1,0,0],
 							[0,0,1,0,0],
-							[0,0,1,0,0]
+							[0,0,1,0,0],
+							[0,0,0,0,0]
 						],
 
 						[
@@ -56,6 +57,7 @@ var CUBE_SHAPE = 	[
 							[0,0,0,0,0],
 							[0,1,1,0,0],
 							[0,1,1,0,0],
+							[0,0,0,0,0],
 							[0,0,0,0,0]
 						]
 					]
@@ -73,7 +75,7 @@ function main(canvas_id) {
 	 var	BOARDWIDTH    		= 10;
 	 var	BOARDHEIGHT   		= 20;
 
-	 var 	HORIZONTALMARGIN 	= ( ( WINDOWWIDTH - (BOARDWIDTH * BOXSIZE)) / 2 );
+	 var 	HORIZONTALMARGIN 	= ( ( WINDOWWIDTH - ( BOARDWIDTH * BOXSIZE )) / 2 );
 	 var	TOPMARGIN			= ( ( WINDOWHEIGHT - ( BOARDHEIGHT * BOXSIZE )) - 5 );
 
 	 var	PIECEWIDTH			= 5;
@@ -87,76 +89,111 @@ function main(canvas_id) {
 
 	 var    BLACK 		= (   0,   0,   0 );
 	 var	WHITE		= ( 255, 255, 255 );
-	 var	RED 		= ( 155,   0,   0 );
+	 var	RED 		= "red";
+
+	 var globalPosition_X = 2; 
+	 var globalPosition_Y = 2;
+		
+	 var 	rotation = I_SHAPE[0];
+	 var canvas = document.getElementById(canvas_id);
+	 var ctx = canvas.getContext("2d");
 
 
-	
+	 	//  function init(canvas_id) { 
+	 		
 
-	 	 function init(canvas_id) { 
-	 		var canvas = document.getElementById(canvas_id);
-	 		var ctx = canvas.getContext("2d");
+	 	//  	// var GameBoard = initGameBoard();
 
-	 	 	var GameBoard = initGameBoard();
+	 	 	
+	  // }
 
-	 	 	ctx.fillStyle = BLACK;
-	 	 	ctx.fillRect(0, 0, WINDOWWIDTH, WINDOWHEIGHT);
+	  // init(canvas_id);
+	  // var board = initGameBoard();
+
+
+	  // main loop
+
+
+	    function piece() {
+	  	this.x = 0;
+	  	this.y = 3;
+	  	this.rotation = rotation;
 	  }
 
-	  init(canvas_id);
-	  var board = initGameBoard();
+	  var testPiece = new piece();
 
-	}	
+	  function update() {
+	  	if ( testPiece.y < 200 ) { 
+	  	testPiece.y += 10;
+
+	  	}
+	  	
+	  }
+
+	  function gameLoop() {
+	  	
+	  	ctx.clearRect(0,0,WINDOWWIDTH,WINDOWHEIGHT);
+	  	ctx.fillStyle = 'black';
+	  	ctx.fillRect(0,0,WINDOWWIDTH,WINDOWHEIGHT);
+	  	update();
+	  	drawPiece(testPiece);
+	  	
+	  	setTimeout(function () {
+	  		requestAnimationFrame(gameLoop);
+	  	}, 1000 * 0.80)
+	  }
+	
+
+	  gameLoop();
+
+	function drawPiece(piece) {
+		for ( let x = 0; x < 5; ++x ) {
+			for ( let y = 0; y < 5; ++y ) {
+				if ( piece.rotation[y][x] != false) {
+					drawFrame( (piece.x * globalPosition_X) + 1, (piece.y * globalPosition_Y) + 1, BOXSIZE * globalPosition_X, BOXSIZE * globalPosition_Y, RED);
+				}
+			}
+		}
+	}
+
+
+
+	function drawFrame(globalX, globalY, x, y, fillstyle) {
+			// window.requestAnimationFrame( drawFrame );
+			// drawing code here
+			
+			// ctx.beginPath();
+			ctx.fillStyle = fillstyle;
+			ctx.fillRect(globalX, globalY, x, y);
+			// ctx.closePath();
+			// ctx.fill();
+			
+			// ctx.fillStyle = fillstyle
+			// ctx.fillRect(globalX, globalY, x, y)	
+	}
 }
+
+
+
+// function getNewPiece() {
+
+// }
 
 function initGameBoard() {
 	var gameBoard = [];
 
 	for ( let i = 0; i < BOARDWIDTH; ++i ) {
 		gameBoard[i] = ( [false] * BOARDHEIGHT );
-
 	return gameBoard;
-}
+	}
+}	
 
 function addPieceToGameBoard(gameBoard, piece) {
-	for ( let x = 0; i < PIECEWIDTH; ++i) {
-		for ( let y = 0; j < PIECEHEIGHT; ++j ) {
-			if ( !piece[y][x] ) {
+	for ( let x = 0; x < PIECEWIDTH; ++x) {
+		for ( let y = 0; y < PIECEHEIGHT; ++y ) {
+			if ( piece[y][x] != false ) {
 				gameBoard[ x + globalPosition_X ][ y + globalPosition_Y ] = currentPieceColor;
 			}
 		}
 	}
 }
-
-function drawPiece(piece) {
-	for ( let x = 0; i < PIECEWIDTH; ++i ) {
-		for ( let y = 0; j < PIECEHEIGHT; ++j ) {
-			if ( !piece[y][x] ) {
-				renderSquare(globalPosition_X, globalPosition_Y, x, y)
-			}
-		}
-	}
-}
-
-function draw() {
-	var FPS = 45;
-	setTimeout( function() {
-		window.requestAnimationFrame( draw );
-		// drawing code here
-	}, 1000 / FPS )
-}
-
-
-var game = new main;
-game.init('tetris_canvas');
-
-
-
-
-
-
-
-
-
-
-
-			
